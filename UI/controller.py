960de4ce5fc9daca2fc87a385201cba0292f9d1a
mdandria05 <mdandria05@gmail.com>
2026-01-13 15:37:26 +0100

    Soluzione PUNTO 1

--- UI/controller.py
+++ UI/controller.py
@@ -10,10 +10,21 @@ class Controller:
     def handle_crea_grafo(self, e):
         """ Handler per gestire creazione del grafo """""
         # TODO
+        G = self._model.create_graph()
+        self._view.dd_squadra.options.clear()
+        self._view.txt_risultato.clean()
+        for team in G.nodes():
+            self._view.dd_squadra.options.append(ft.DropdownOption(key=team.id, text=f"{team.team_code} ({team.name})"))
+        self._view.update()
 
     def handle_dettagli(self, e):
         """ Handler per gestire i dettagli """""
         # TODO
+        self._view.txt_risultato.controls.clear()
+        edges = self._model.get_edges(self._view.dd_squadra.value)
+        for edge,weight in edges:
+            self._view.txt_risultato.controls.append(ft.Text(f"{edge.team_code} ({edge.name}), peso {weight}"))
+        self._view.update()
 
     def handle_percorso(self, e):
         """ Handler per gestire il problema ricorsivo di ricerca del percorso """""
@@ -27,6 +38,7 @@ class Controller:
         for a in range(value[0], value[1]+1):
             lista.append(ft.DropdownOption(key=a,content=ft.Text(f"{a}")))
         return lista
+
     def update_lista1(self,e):
         value = self._view.dd_anno.value
         self._view.txt_out_squadre.clean()
@@ -35,4 +47,5 @@ class Controller:
         for team in teams.values():
             self._view.txt_out_squadre.controls.append(ft.Text(f"{team.team_code} ({team.name})"))
         self._view.update()
+
 
--- model/model.py
+++ model/model.py
@@ -1,18 +1,36 @@
 from database.dao import DAO
+import networkx as nx
 class Model:
     def __init__(self):
         self.anno = None
         self.team_dict = {}
+        self.G = nx.Graph()
+
     def ricerca_anni(self):
         anni = DAO.get_anni()
         return anni
+
     def ricerca_team_anno(self,anno):
         self.anno = anno
-        self.team_dict = {}
+        self.team_dict.clear()
         self.team_list = []
         self.team_list = DAO.get_squadre_anno(self.anno)
         for team in self.team_list:
             self.team_dict[team.id] = team
         return self.team_dict
 
+    def create_graph(self):
+        self.G.clear()
+        for team1 in self.team_list:
+            for team2 in self.team_list:
+                if team1 != team2 and not self.G.has_edge(team1, team2):
+                    self.G.add_edge(team1, team2, weight=team1.tot_salary + team2.tot_salary)
+        return self.G
+
+    def get_edges(self, node):
+        edges = []
+        for u, v, weight in self.G.edges(self.team_dict[int(node)], data=True):
+            edges.append((v, weight['weight']))
+        edges.sort(key=lambda x: x[1], reverse=True)
+        return edges
 
